aacc26e00b2e8f5587e490fe21d0fc836a59d537
yasinabdmahmood <abd181103@gmail.com>
2022-11-13 01:45:56 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import rough from 'roughjs/bundled/rough.esm';
 import React, { useLayoutEffect, useState } from 'react';
 import './App.css';
 
-
 const generator = rough.generator();
 function App() {
   const [elements, setElements] = useState([]);
   const [action, setAction] = useState('none');
   const [tool, setTool] = useState('line');
-  const [selectedElement,setSelectedElement] = useState(null)
+  const [selectedElement, setSelectedElement] = useState(null);
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas');
     const roughCanvas = rough.canvas(canvas);
@@ -18,51 +17,51 @@ function App() {
       roughCanvas.draw(roughElement);
     });
   });
-  const distance = (a,b) => {
-   return Math.sqrt(Math.pow((a.x-b.x),2) + Math.pow((a.y-b.y),2))
-  }
+  const distance = (a, b) => Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
   const isWithinElement = (x, y, element) => {
-    const {x1,y1,x2,y2,type} = element;
-    if(type === 'rectangle'){
-      const minX = Math.min(x1,x2);
-      const maxX = Math.max(x1,x2);
-      const minY = Math.min(y1,y2);
-      const maxY = Math.max(y1,y2);
+    const {
+      x1, y1, x2, y2, type,
+    } = element;
+    if (type === 'rectangle') {
+      const minX = Math.min(x1, x2);
+      const maxX = Math.max(x1, x2);
+      const minY = Math.min(y1, y2);
+      const maxY = Math.max(y1, y2);
       return x >= minX && x <= maxX && y >= minY && y <= maxY;
-    } else {
-      const a = {x:x1,y:y1};
-      const b = {x:x2,y:y2};
-      const c = {x,y};
-      const offset = distance(a,b) - (distance(a,c) + distance(b,c))
-      return Math.abs(offset) < 1;
     }
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    return Math.abs(offset) < 1;
   };
-  const updateElement = (id,x1, y1, x2, y2, type) => {
-    const updatedElement = createElement(id,x1, y1, x2, y2, type);
-    const copyElements = [...elements];
-    copyElements[id] = updatedElement;
-    setElements(copyElements);
-  }
-  const getElementAtPosition = (x, y, elements) => elements.find(
-    (element) => isWithinElement(x, y, element),
-  );
   const createElement = (id, x1, y1, x2, y2, type) => {
     const roughElement = type === 'line'
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
     return {
-      id,x1, y1, x2, y2, type, roughElement,
+      id, x1, y1, x2, y2, type, roughElement,
     };
   };
+  const updateElement = (id, x1, y1, x2, y2, type) => {
+    const updatedElement = createElement(id, x1, y1, x2, y2, type);
+    const copyElements = [...elements];
+    copyElements[id] = updatedElement;
+    setElements(copyElements);
+  };
+  const getElementAtPosition = (x, y, elements) => elements.find(
+    (element) => isWithinElement(x, y, element),
+  );
+
   const handleMouseDown = (e) => {
     const { clientX, clientY } = e;
     if (tool === 'selection') {
       const element = getElementAtPosition(clientX, clientY, elements);
-      if(element){
+      if (element) {
         const offsetX = clientX - element.x1;
         const offsetY = clientY - element.y1;
-        setSelectedElement({...element, offsetX, offsetY})
-        setAction('moving')
+        setSelectedElement({ ...element, offsetX, offsetY });
+        setAction('moving');
       }
     } else {
       const id = elements.length;
@@ -77,21 +76,21 @@ function App() {
       const index = elements.length - 1;
       const lastElement = elements[index];
       const { x1, y1 } = lastElement;
-      updateElement(index,x1, y1, clientX, clientY, tool);
-      
-    } else if(action === 'moving') {
-      const {id,x1,x2,y1,y2,type,offsetX,offsetY} = selectedElement;
+      updateElement(index, x1, y1, clientX, clientY, tool);
+    } else if (action === 'moving') {
+      const {
+        id, x1, x2, y1, y2, type, offsetX, offsetY,
+      } = selectedElement;
       const width = x2 - x1;
       const height = y2 - y1;
       const newX1 = clientX - offsetX;
-      const newY1 = clientY - offsetY
-      updateElement(id,newX1, newY1, newX1 + width, newY1 + height, type);
-
+      const newY1 = clientY - offsetY;
+      updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type);
     }
   };
   const handleMouseUp = () => {
     setAction('none');
-    setSelectedElement(null)
+    setSelectedElement(null);
   };
   return (
     <div>
