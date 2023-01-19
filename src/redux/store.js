260113704459728232cffebe7945ce0699b43a1c
yasinabdmahmood <abd181103@gmail.com>
2023-01-19 17:46:40 +0300

    redux related code

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './stateReducer'
-
-export const store = configureStore({
-  reducer: {
-    canvas: counterReducer,
-  },
-})
