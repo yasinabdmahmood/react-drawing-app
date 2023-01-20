6f5fbc756981287bff7942d8e53697ff3dd52816
yasinabdmahmood <abd181103@gmail.com>
2023-01-20 15:40:52 +0300

    fix linter errors

--- src/components/DrawingApp.js
+++ src/components/DrawingApp.js
@@ -1,36 +1,40 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Doodle from '../classes/Doodle';
 
 function DrawingApp() {
   const [lines, setLines] = useState([]);
   const [undoStack, setUndoStack] = useState([]);
   const [isDrawing, setIsDrawing] = useState(false);
 
-  const handleMouseDown = e => {
+  const handleMouseDown = (e) => {
     setIsDrawing(true);
-    setLines(lines.concat([[{ x: e.clientX, y: e.clientY }]]));
+    const startingPoint = { x: e.clientX, y: e.clientY };
+    const doodle = new Doodle(startingPoint);
+    setLines((prev) => [...prev, doodle]);
   };
 
-  const handleMouseMove = e => {
+  const handleMouseMove = (e) => {
     if (isDrawing) {
-      setLines(lines.slice(0, -1).concat([lines.slice(-1)[0].concat({ x: e.clientX, y: e.clientY })]));
+      const last = lines[lines.length - 1];
+      last.addPoint({ x: e.clientX, y: e.clientY });
+      setLines((prev) => prev.slice(0, -1).concat(last));
     }
   };
 
   const handleMouseUp = () => {
-    console.log(lines)
     setIsDrawing(false);
     setUndoStack([]);
   };
 
-  const handleUndo = (event) => {
-    
+  const handleUndo = () => {
     if (lines.length > 0) {
       setUndoStack(undoStack.concat([lines.slice(-1)[0]]));
       setLines(lines.slice(0, -1));
     }
   };
 
-  const handleRedo = (event) => {
+  const handleRedo = () => {
     if (undoStack.length > 0) {
       setLines(lines.concat([undoStack.slice(-1)[0]]));
       setUndoStack(undoStack.slice(0, -1));
@@ -39,21 +43,23 @@ function DrawingApp() {
 
   return (
     <div>
-        <div style={{position:'absolute',left:'0%',top:'90%'}}>
-        <button onClick={handleUndo} >Undo</button>
-        <button onClick={handleRedo} >Redo</button>
-        </div>
-      
+      <div style={{ position: 'absolute', left: '0%', top: '90%' }}>
+        <button type="button" onClick={handleUndo}>Undo</button>
+        <button type="button" onClick={handleRedo}>Redo</button>
+      </div>
+
       <svg
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
-        style={{position:'absolute',left:'0%',top:'0%', width:'100vw', height:'90vh',border: '1px solid black',boxSizing: 'border-box'}}
+        style={{
+          position: 'absolute', left: '0%', top: '0%', width: '100vw', height: '90vh', border: '1px solid black', boxSizing: 'border-box',
+        }}
       >
-        {lines.map((line, index) => (
+        {lines.map((line) => (
           <path
-            key={index}
-            d={line.reduce((acc, point) => acc + ` L ${point.x} ${point.y}`, `M ${line[0].x} ${line[0].y}`)}
+            key={uuidv4()}
+            d={line.draw()}
             stroke="black"
             fill="none"
           />
