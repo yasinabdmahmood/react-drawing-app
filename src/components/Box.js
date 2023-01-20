bf9d1e259fc9f71e34443c82cf89ac59cf725dd2
yasinabdmahmood <abd181103@gmail.com>
2023-01-21 00:25:16 +0300

    Add color picker

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { CompactPicker } from 'react-color';
+import { addDrawing, deleteLastDrawing } from '../redux/DrawingsReducer';
+import { changeColor } from '../redux/configReducer';
+import { addToUndoStack, removeFromUndoStack } from '../redux/undoStackReducer';
 
-const Box = ({ width, height }) => {
+const Box = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
   const [initialPosition, setInitialPosition] = useState({ x: 0, y: 0 });
+  const undoStack = useSelector((state) => state.undoStack);
+  const lines = useSelector((state) => state.drawings);
+  const currentColor = useSelector((state) => state.configurations.color);
+  const dispatch = useDispatch();
 
   const handleMouseDown = (e) => {
+    if (e.target !== e.currentTarget) return;
     setInitialPosition({ x: e.clientX, y: e.clientY });
     setIsDragging(true);
   };
 
   const handleMouseMove = (e) => {
+    if (e.target !== e.currentTarget) return;
     if (!isDragging) {
       return;
     }
@@ -25,22 +36,50 @@ const Box = ({ width, height }) => {
     setIsDragging(false);
   };
 
+  const handleUndo = () => {
+    if (lines.length > 0) {
+      // setUndoStack(undoStack.concat([lines.slice(-1)[0]]));
+      // setLines(lines.slice(0, -1));
+      dispatch(addToUndoStack(lines[lines.length - 1]));
+      dispatch(deleteLastDrawing());
+    }
+  };
+
+  const handleRedo = () => {
+    if (undoStack.length > 0) {
+      dispatch(addDrawing(undoStack.slice(-1)[0]));
+      // setUndoStack(undoStack.slice(0, -1));
+      dispatch(removeFromUndoStack());
+    }
+  };
+
+  const handleColorChnange = (color) => {
+    dispatch(changeColor(color.hex));
+  };
+
   return (
     <div
       style={{
-        width,
-        height,
+        width: '245px',
+        height: '250px',
         backgroundColor: 'gray',
-        border: isDragging? '2px solid blue' : '1px solid black',
-        position: 'relative',
+        border: isDragging ? '2px solid blue' : '1px solid black',
+        position: 'absolute',
         left: position.x,
         top: position.y,
-        zIndex: '2',
+        zIndex: '1',
       }}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-    />
+    >
+      <button type="button" onClick={handleUndo}>Undo</button>
+      <button type="button" onClick={handleRedo}>Redo</button>
+      <CompactPicker
+        color={currentColor}
+        onChangeComplete={handleColorChnange}
+      />
+    </div>
   );
 };
 
