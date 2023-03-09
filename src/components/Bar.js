41c3e461e2d2c8eea8fa9b7a7cbe54573ead99cb
yasinabdmahmood <abd181103@gmail.com>
2023-03-09 22:01:27 +0300

    Fix linter errors

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -4,16 +4,16 @@ import { changeShape } from '../redux/configReducer';
 
 /// /// import images
 import style from '../assets/styles/Bar.module.css';
-import cursor from '../assets/images/cursor.svg';
+// import cursor from '../assets/images/cursor.svg';
 import square from '../assets/images/square.svg';
 import diamond from '../assets/images/diamond.svg';
 import circle from '../assets/images/circle.svg';
 import arrow from '../assets/images/arrow-right.svg';
 import dash from '../assets/images/dash-lg.svg';
 import pencil from '../assets/images/pencil.svg';
-import text from '../assets/images/fonts.svg';
-import image from '../assets/images/card-image.svg';
-import eraser from '../assets/images/eraser.svg';
+// import text from '../assets/images/fonts.svg';
+// import image from '../assets/images/card-image.svg';
+// import eraser from '../assets/images/eraser.svg';
 /// ///////////////////////////////////////////////
 
 function Bar() {
