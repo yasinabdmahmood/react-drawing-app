0f7126ae4dce5822daf1ad5fb38f8cadf1b2fc36
yasinabdmahmood <abd181103@gmail.com>
2023-11-08 19:10:39 +0300

    Add label including my name to the main page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App">
       <Bar />
       <DrawingApp />
+      <div className="label">
+        <h3>Developed by : Eng. Yaseen abdulwahid</h3>
+        <h3>College : Computer Science</h3>
+        <h3>Department : programming</h3>
+        <br />
+      </div>
     </div>
   );
 }
